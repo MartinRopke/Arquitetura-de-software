72f4377551a4500d88c850044dabd7834985cf01
martinropke <martin.ropke@gmail.com>
2023-08-23 08:31:09 -0300

    Create script to build docker images

---
+++ DockerBuild.cmd
@@ -0,0 +1,5 @@
+start /B docker build -t barramento-de-eventos ./barramento-de-eventos
+start /B docker build -t classificacao ./classificacao
+start /B docker build -t consulta ./consulta
+start /B docker build -t lembretes ./lembretes
+start /B docker build -t observacoes ./observacoes
